af1ae00d577d1da64e54ce5a569c94c649211a0c
Chris <christopher.emrick.2022@student.lhschools.org>
2019-04-23 13:57:12 -0400

    Optimized switch and added leap year logic

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -20,7 +20,7 @@ public class Main {
             int day = scan.nextInt();
             System.out.println("You were born on a " + dayOfTheWeek(month, day, year) + ".");
             System.out.println("You were born in " + monthString + ".");
-            System.out.println(monthString + " has " + getNumberOfDays(month) + " days in it.");
+            System.out.println(monthString + " has " + getNumberOfDays(month, year) + " days in it.");
             getRhyme(dayOfTheWeek(month, day, year));
             System.out.println("Type y if you want to try again and type n if you don't.");
             scan.nextLine();
@@ -91,32 +91,27 @@ public class Main {
         return "Invalid month";
 
     }
-    private static int getNumberOfDays(int month) {
+    private static int getNumberOfDays(int month, int year) {
         switch(month) {
             case 1:
-                return 31;
-            case 2:
-                return 28;
             case 3:
-                return 31;
-            case 4:
-                return 30;
             case 5:
-                return 31;
-            case 6:
-                return 30;
             case 7:
-                return 31;
             case 8:
-                return 31;
-            case 9:
-                return 30;
             case 10:
-                return 31;
-            case 11:
-                return 30;
             case 12:
                 return 31;
+            case 2: // leap year
+                // logic here
+                if((year % 4) == 0) {
+                    return 29;
+                }
+                return 28;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                return 30;
         }
         return 0;
     }
